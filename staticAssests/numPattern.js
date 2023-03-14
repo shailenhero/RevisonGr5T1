186442817467ee0395f04338c4caa19f7964360b
shailenhero <shailenhero@gmail.com>
2023-03-14 18:09:11 +0200

    math

---
+++ staticAssests/numPattern.js
@@ -0,0 +1,11 @@
+console.log("working")
+const jasonData = document.querySelector("#data");
+const questionDisplay = document.querySelector(".question");
+const button = document.querySelector('#button')
+
+button.addEventListener('click', function () {
+    console.log("btn clicked")
+
+
+})
+
